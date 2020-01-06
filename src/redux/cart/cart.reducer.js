41b9266d3c153c78b658cf5d60554a532f2a94ef
philippe woerth <phil67b@gmail.com>
2020-01-06 21:10:44 +0100

    updated shop data with new data, updated card utils to group items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from './cart.types';
-
+import { addItemToCard } from './cart.utils'
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,7 +12,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             };
-        default:
+        case CartActionTypes.ADD_ITEM :
+            return {
+                ...state,
+                cartItems : addItemToCard(state.cartItems, action.payload)
+
+            };
+            default:
             return state;
     }
 };
